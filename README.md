ed0a31ac19f1a4a92a437d38998012cc117204f0
zhangckcup <zhangckcup@qq.com>
2023-03-01 22:52:13 +0800

    ConcurrentModeAndFiber

--- README.md
+++ README.md
@@ -28,46 +28,14 @@ MyReact.render(element, root);
 TODO: 只要开始递归便不会停止，可能会阻塞浏览器，寻找优化方法；
 
 ```TextPlain
-React 工作顺序：
-
+React 工作循环：
 判断浏览器是否有空余的时间？
-
 判断是否存在下一个任务单元
-
 执行工作单元
 ```
 
 ```TextPlain
 fiber 架构：一种组织工作单元的树状数据结构
-```
-
-```js
-let nextUnitOfWork = null;
-
-// fiber 架构：一种组织工作单元的树状数据结构
-function preforUnitOfWork(unitOfWork) {
-  // 执行任务单元
-
-  //  return 下一个任务单元
-}
-
-// 判断浏览器是否有空余的时间？
-// 判断是否存在下一个任务单元
-// 执行工作单元
-function workLoop(deadline) {
-  let shouldYield = true;
-
-  while (nextUnitOfWork && shouldYield) {
-    nextUnitOfWork = preforUnitOfWork(nextUnitOfWork);
-    shouldYield = timeRemaining() > 1;
-  }
-
-  deadline.timeRemaining(); // 得到浏览器当前帧剩余的时间  scheduler
-
-  requestIdleCallback(workLoop);
-}
-
-requestIdleCallback(workLoop);
 ```
 
 ## renderAndCommit
--- src/MyReact/render.js
+++ src/MyReact/render.js
@@ -1,4 +1,4 @@
-function render(element, container) {
+function createDom(element) {
   const dom =
     element.type === "TEXT"
       ? document.createTextNode(element.props.nodeValue)
@@ -9,10 +9,83 @@ function render(element, container) {
     .filter(isProperty)
     .forEach((name) => (dom[name] = element.props[name]));
 
-  // TODO: 只要开始递归便不会停止，可能会阻塞浏览器
-  element?.props?.children?.forEach((child) => render(child, dom));
+  return dom;
+}
 
-  container.appendChild(dom);
+let nextUnitOfWork = null;
+
+// 执行任务单元
+// fiber 架构：一种组织工作单元的树状数据结构
+function preforUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // 为当前的fiber创造子节点的fiber   fiber.child? === new;
+  // parnet child sibling
+  const children = fiber?.props?.children;
+
+  // 用于追加兄弟节点
+  let preSibling = null;
+  children.forEach((childElement, index) => {
+    const newFiber = {
+      parent: fiber,
+      props: childElement.props,
+      type: childElement.type,
+      dom: null,
+      // sibling: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      preSibling.sibling = newFiber;
+    }
+
+    preSibling = newFiber;
+  });
+
+  // 深度优先遍历，寻找下一个任务单元
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  // 寻找兄弟节点
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
+}
+
+
+function workLoop(deadline) {
+  let shouldYield = true;
+
+  // 判断是否存在下一个任务单元 && 浏览器是否有空余的时间
+  while (nextUnitOfWork && shouldYield) {
+    nextUnitOfWork = preforUnitOfWork(nextUnitOfWork);  // 执行工作单元
+    shouldYield = deadline.timeRemaining() > 1;         // 得到浏览器当前帧剩余的时间  scheduler
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
 }
 
 export default render;
