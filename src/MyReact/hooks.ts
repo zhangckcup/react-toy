3d10ea4d55df97e7102b26c703a502acee1666d0
zhangckcup <zhangckcup@qq.com>
2023-03-12 17:11:28 +0800

    fix

---
+++ src/MyReact/hooks.ts
@@ -0,0 +1,14 @@
+export function useEffect(callback, deps) {
+  
+}
+
+export function useMome<T>(nextCreate: () => T, deps: Array<any> | void | null) {
+  // const hook = updateWorkInProgressHook();
+  // const nextDeps = deps ?? null;
+
+  // const nextValue = nextCreate();
+
+  // hook.memoizedState = [nextCreate, nextDeps];
+  // return nextValue;
+
+}
--- src/MyReact/render.js
+++ src/MyReact/render.js
@@ -1,5 +1,3 @@
-import { updateFunctionComponent } from "./hooks.js";
-
 function createDom(fiber) {
   const dom =
     fiber.type === "TEXT"
@@ -105,6 +103,48 @@ function reconcileChildren(wipFiber, elements) {
     preSibling = newFiber;
     index++;
   }
+}
+
+let wipFiber;
+let hooksIndex;
+
+export function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  wipFiber.hooks = [];
+  hooksIndex = 0;
+
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+export function useState(initVal) {
+  const oldHook = wipFiber?.alternate?.hooks?.[hooksIndex];
+
+  const hook = {
+    state: oldHook ? oldHook.state : initVal,
+    queue: [],
+  }
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach((action) => {
+    hook.state = action;
+  })
+
+  const setState = action => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  }
+
+  wipFiber.hooks.push(hook);
+  hooksIndex++;
+
+  return [hook.state, setState]
 }
 
 function updateHostComponents(fiber) {
